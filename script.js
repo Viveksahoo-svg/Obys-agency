e5e42699d014723caf33d2fbe0d056302892ad90
Vivek Sahoo <viveksahoo96@gmail.com>
2024-02-13 12:34:05 +0530

    page1 done

--- script.js
+++ script.js
@@ -8,37 +8,38 @@ function loadingAnimation() {
     delay: 0.5,
   });
 
-  tl.from("#line1-part1", {
+  tl.from("#line1-part1, .line h2", {
     opacity: 0,
     onStart: () => {
       var h5timer = document.querySelector("#line1-part1 h5");
       var grow = 0;
       setInterval(() => {
         if (grow < 100) {
+          grow++;
           h5timer.innerHTML = grow++;
         } else {
           h5timer.innerHTML = grow;
         }
-      }, 33);
+      }, 30);
     },
   });
 
-  tl.to(" .line h2", {
+  tl.to(".line h2", {
     animationName: "anime",
-    opacity: 0,
+    opacity: 1,
   });
 
   tl.to("#loader", {
     opacity: 0,
-    duration: 0.2,
-    delay: 3,
+    duration: 0.6,
+    delay: 2.5,
   });
 
   tl.from("#page1", {
     y: 1600,
     opacity: 0,
     delay: 0.2,
-    duration: 0.6,
+    duration: 0.5,
     ease: Power4,
   });
 
@@ -48,11 +49,11 @@ function loadingAnimation() {
 
   tl.from("#nav", {
     opacity: 0,
-  });
+  })
 
   tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero3 h3, #hero4 h1",{
-    y:120,
-    stagger: 0.2
+     y: 120,
+     stagger:0.2
   })
 }
 
@@ -63,7 +64,7 @@ function cursorAnimation() {
       top: dets.y,
     });
   });
-  
+
   Shery.makeMagnet("#nav-part2 h4" /* Element to target.*/, {
     //Parameters are optional.
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
